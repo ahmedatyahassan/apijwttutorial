9848d56a2472f4a6c8dbfd5f3f37d8d970925e7d
AhmedAtya74 <aatya8000@gmail.com>
2023-12-20 15:22:42 +0200

    Use uuid instead of id in adress class.

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
     use HasFactory;
-
+    use HasUuids;
     protected $fillable = ['user_id','country'];
 
     protected $hidden = [
